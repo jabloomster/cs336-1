59b4f9950609ced79d07c8af2d42be1215f9bec2
Josh Maguire <jlm54@students.calvin.edu>
2016-11-04 13:56:20 -0400

    Submit lab9

--- lab9/webpack.config.js
+++ lab9/webpack.config.js
@@ -1,20 +1,30 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin')
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: [
-        __dirname + '/app/scripts/index.js'
+    entry: [    //source code modules
+    __dirname + '/app/scripts/index.js'
     ],
-    output: {
+    output: {       //destination for bundle
         path: __dirname + '/dist',
         filename: '/bundle.js'
     },
-    module: {
-        loaders: [
-            { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader" },
-            { test: /\.css$/,  loader: 'style!css?modules!postcss' }
+    module: {       
+        loaders: [  //preprocessors
+        { test: /\.jsx?$/, exclude: /node_modules/, loader: "babel-loader" },
+        { test: /\.css$/,  loader: 'style!css?modules!postcss' }
         ]
     },
     plugins: [
-    new HtmlWebpackPlugin({template: __dirname + "/app/index.tmpl.html"})
-    ]
+    new HtmlWebpackPlugin({template: __dirname + "/app/index.tmpl.html"}),
+    new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        port: 3001,
+        proxy: { '/api/*': 'http://localhost:3000' },
+        colors: true,
+        historyApiFallback: true,
+        inline: true,
+        hot: true
+    }
 };
