39045951e1cfa1f044140794d6237f05538fe5f7
Josh Maguire <jlm54@students.calvin.edu>
2016-12-06 13:58:59 -0500

    lab13 production fixes

--- app/scripts/flux.js
+++ app/scripts/flux.js
@@ -4,22 +4,20 @@ import { createStore } from 'redux';
 import { API_URL, POLL_INTERVAL } from './global';
 
 let StoreTools = {
-	let StoreTools = {
-		dispatchLoadingComments: function () {
-			store.dispatch(ActionTools.loadingComments());
-		},
-		startLoadingComments: function() {
-			this.dispatchLoadingComments();
-			setInterval(this.dispatchLoadingComments, POLL_INTERVAL);
-		},
-		findComment: function(id, commentList) {
-			for (var comment of commentList) {
-				if (comment.id == id) {
-					return { id: id, author: comment.author, text: comment.text };
-				}
+	dispatchLoadingComments: function () {
+		store.dispatch(ActionTools.loadingComments());
+	},
+	startLoadingComments: function() {
+		this.dispatchLoadingComments();
+		setInterval(this.dispatchLoadingComments, POLL_INTERVAL);
+	},
+	findComment: function(id, commentList) {
+		for (var comment of commentList) {
+			if (comment.id == id) {
+				return { id: id, author: comment.author, text: comment.text };
 			}
-			return { id: '',  author: '', text: '' };
 		}
+		return { id: '',  author: '', text: '' };
 	}
 };
 
